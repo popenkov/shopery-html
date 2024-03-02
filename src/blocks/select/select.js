bc9fb2e2e6a4931402561724eccbbe08577b433e
Tony Popenkov <popenkovaa@yandex.ru>
2024-03-02 19:05:23 +0300

    feat: WIP catalog page

---
+++ src/blocks/select/select.js
@@ -0,0 +1,6 @@
+import "nice-select2";
+import ready from "../../js/utils/documentReady.js";
+
+ready(function () {
+  const selectInstance = NiceSelect.bind(document.querySelector(".js-nice-select"));
+});
