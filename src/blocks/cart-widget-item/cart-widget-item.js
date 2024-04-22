35795ae26e2b8b653e66248cca60c0da3ddd06a2
Tony Popenkov <popenkovaa@yandex.ru>
2024-04-22 17:47:03 +0300

    feat(WIP): work on cart widget

---
+++ src/blocks/cart-widget-item/cart-widget-item.js
@@ -0,0 +1,5 @@
+// import ready from "../../js/utils/documentReady.js";
+
+// ready(function () {
+//   your code goes here
+// });
--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -6,13 +6,5 @@ ready(function () {
     linkAttributeName: "data-hystmodal",
     catchFocus: true,
     waitTransitions: true,
-    beforeOpen: function (modal) {
-      console.log("Message before opening the modal");
-      console.log(modal); //modal window object
-    },
-    afterClose: function (modal) {
-      console.log("Message after modal has closed");
-      console.log(modal); //modal window object
-    },
   });
 });
