f382f4c42548853fbfdfc271fab1a047ca03d631
Tony Popenkov <popenkovaa@yandex.ru>
2024-04-23 10:06:20 +0300

    feat: modal adaptive added

--- src/blocks/product-preview/product-preview.js
+++
@@ -1,9 +0,0 @@
-import ready from "../../js/utils/documentReady.js";
-
-ready(function () {
-  const itemPreview = document.querySelector(".product-preview");
-
-  if (itemPreview) {
-    window.myModal.open("#productPreview");
-  }
-});
