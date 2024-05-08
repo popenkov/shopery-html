e43ce1db957b312619a78108999d9c07db793cd7
Tony Popenkov <popenkovaa@yandex.ru>
2024-05-08 09:06:59 +0300

    feat: sign in scripts added

--- src/blocks/sign-in/sign-in.js
+++ src/blocks/sign-in/sign-in.js
@@ -1,5 +1,45 @@
-// import ready from "../../js/utils/documentReady.js";
+import JustValidate from "just-validate";
 
-// ready(function () {
-//   your code goes here
-// });
+// todo не подключается скрипт страницы через сборщик
+export const initSignInScripts = () => {
+  const signinPage = document.querySelector(".sign-in");
+  if (!signinPage) {
+    return;
+  }
+
+  const signinForm = signinPage.querySelector(".js-signin-form");
+  const formInputs = signinForm.querySelectorAll("input");
+
+  const validate = new JustValidate(signinForm, {
+    lockForm: true,
+  });
+
+  validate
+    .addField("#signin-email", [
+      {
+        rule: "required",
+      },
+      {
+        rule: "email",
+      },
+    ])
+    .addField("#signin-password", [
+      {
+        rule: "required",
+      },
+    ]);
+
+  formInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      validate.revalidateField(`#${input.getAttribute("id")}`);
+    });
+  });
+
+  signinForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const isFormValid = validate.isValid;
+    if (isFormValid) {
+      console.log("form sent");
+    }
+  });
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import { initRegistrationScripts } from "../blocks/registration/registration.js";
+import { initSignInScripts } from "../blocks/sign-in/sign-in.js";
 import ready from "../js/utils/documentReady.js";
 import getScrollSize from "../js/utils/getScrollSize.js";
 
@@ -6,6 +7,7 @@ ready(function () {
   // Adding a custom property with a system scroll width
   document.documentElement.style.setProperty("--css-scroll-size", `${getScrollSize()}px`);
   initRegistrationScripts();
+  initSignInScripts();
 });
 
 // import $ from "jquery"; // Install as dependency before use
